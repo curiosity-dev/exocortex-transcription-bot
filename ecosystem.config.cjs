0ec9fac6f64b0c070975450b344f977c56527113
Evgeniy Karavaev <karavaev.evgeniy@gmail.com>
2024-10-26 23:33:20 +0800

    Fixed multiply messages

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,8 @@ module.exports = {
         name: "exocortex-transcription-bot",
         script: "bun",
         args: "app/index.ts",
-        watch: true,
+        watch: false, // отключаем watch
+        max_memory_restart: "1G",
         env: {
             NODE_ENV: "production"
         }
